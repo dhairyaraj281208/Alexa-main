d7980b0b384f228f6d0b90ba6b8c59fd80256dbf
dhairyaraj281208 <72195034+dhairyaraj281208@users.noreply.github.com>
2021-05-12 11:09:56 +0530

    Add files via upload

---
+++ sabji.py
@@ -0,0 +1,52 @@
+from jarvis import speak
+
+def sabji():
+        import random
+        rand = 8
+        if(rand == 1):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 2):
+            speak("aajj Tindi ki sabji banaa lena")
+        if(rand == 3):
+            speak("Chalo bhindi ki sabji banaa looo")
+        if(rand == 4):
+            speak("aajj Aalu pyaazz ki sabji banaa lena")
+        if(rand == 5):
+            speak("chalo aajj Aalu paannir ki sabji banaa loo")
+        if(rand == 6):
+            speak("chalo aajj Aalu paalak ki sabji banaa loo")
+        if(rand == 7):
+            speak("chalo aajj aalu choolle ki sabji banaa loo")
+        if(rand == 8):
+            speak("chalo aajj Paalak Pannir ki sabji banaa lena")
+        if(rand == 9):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 10):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 11):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 12):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 13):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 14):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 15):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 16):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 17):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 18):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 19):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 20):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 21):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 22):
+            speak("chalo aajj Turai ki sabji banaa lena")
+        if(rand == 23):
+            speak("chalo aajj Turai ki sabji banaa lena")
+sabji()
